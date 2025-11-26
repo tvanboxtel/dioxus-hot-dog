20397b5a62853061788c0dea799b89ed55832b07
Teun van Boxtel <teunvboxtel@protonmail.com>
2025-11-26 09:20:56 +0100

    feat: db integration

--- src/main.rs
+++ src/main.rs
@@ -25,19 +25,34 @@ fn App() -> Element {
     }
 }
 
+// The database is only available to server code
+#[cfg(feature = "server")]
+thread_local! {
+    pub static DB: rusqlite::Connection = {
+        // Open the database from the persisted "hotdog.db" file
+        let conn = rusqlite::Connection::open("hotdog.db").expect("Failed to open database");
+
+        // Create the "dogs" table if it doesn't already exist
+        conn.execute_batch(
+            "CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                url TEXT NOT NULL
+            );",
+        ).unwrap();
+
+        // Return the connection
+        conn
+    };
+}
+
+
+
+
+
+
 #[post("/api/save_dog")]
 async fn save_dog(image: String) -> Result<()> {
-    use std::io::Write;
-
-    let mut file = std::fs::OpenOptions::new()
-        .write(true)
-        .append(true)
-        .create(true)
-        .open("dogs.txt")
-        .unwrap();
-
-    file.write_fmt(format_args!("{image}\n"));
-
+    DB.with(|db| db.execute("INSERT INTO dogs (url) VALUES (?1)", [&image]))?;
     Ok(())
 }
 
